e4e4cc14a9537f455b3ef45517bf3398e3cc3f32
lxx1884896 <1523631706@qq.com>
2018-09-21 15:08:26 +0800

    123
    111

--- 1111111111111111.py
+++ 1111111111111111.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-import os
+# import os
+#
+# a=os.path.split('/home/swaroop/byte/code/poem.txt')
+# print a
+# #print a.split(".")[0]
 
-a=os.path.split('/home/swaroop/byte/code/poem.txt')[1]
-print a.split(".")[0]
+dict ={'f':2,'b':3,'c':1}
+
+
+#print max(dict)
+
+dict={dict[key]:key for key in dict}#   dict为{1: 'c', 2: 'f', 3: 'b'}
+
+print max(dict,key=dict.get)#得到最大值所对应的键
+print dict
